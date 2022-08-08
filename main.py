a88b904419ce1a3dd9ba6a2aafc062286b5c7184
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-08 20:12:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,137 +72,6 @@ OWNER_STICKER = ["CAACAgUAAxkBAAEFeKVi7KQ03IWzIsadwiDmClcqqR1VdAAC7QYAAgxtuFUmtw
                 "CAACAgUAAxkBAAEFeKVi7KQ03IWzIsadwiDmClcqqR1VdAAC7QYAAgxtuFUmtwiAlNzfTykE"             
          ]
 
-
-class Message:
-    def __init__(self, id, mfrom, subject, date, body, text, html):
-        self.id = id
-        self.mfrom = mfrom
-        self.subject = subject
-        self.date = date
-        self.body = body
-        self.text = text
-        self.html = html
-
-class OneSecMailApi:
-    def __init__(self):
-        self.base_url = 'https://www.1secmail.com/api/v1/'
-        self.email = None
-        self.login = None
-        self.domain = None
-        self.mailbox = []
-
-    def getUrl(self, url):
-        if (pdomain := environ.get("PROXY_DOMAIN")) and (pkey := environ.get("PROXY_KEY")):
-            return f"https://{pdomain}/?url={quote(url)}&key={pkey}"
-        return url
-
-    async def get_mail(self):
-        try:
-            async with ClientSession() as session:
-                async with session.get(self.getUrl(f'{self.base_url}/?action=genRandomMailbox&count=10')) as resp:
-                    mail = choice(await resp.json())
-                    self.email = mail
-                    self.login, self.domain = mail.split("@")
-                    return mail
-        except Exception as e:
-            print(f"{e.__class__.__name__}: {e!s}")
-            return None
-    
-    async def fetch_inbox(self):
-        async with ClientSession() as session:
-            async with session.get(self.getUrl(f'{self.base_url}?action=getMessages&login={self.login}&domain={self.domain}')) as resp:
-                for message in await resp.json():
-                    if [m for m in self.mailbox if m.id == message["id"]]:
-                        continue
-                    self.mailbox.append(await self.get_message(message["id"]))
-        return self.mailbox.copy()
-    
-    async def get_message(self, message_id):
-        async with ClientSession() as session:
-            async with session.get(self.getUrl(f'{self.base_url}?action=readMessage&login={self.login}&domain={self.domain}&id={message_id}')) as resp:
-                j = await resp.json()
-                msg = Message(
-                    id=j.get("id"),
-                    mfrom=j.get("from"),
-                    subject=j.get("subject"),
-                    date=datetime.fromisoformat(j.get("date")),
-                    body=j.get("body"),
-                    text=j.get("textBody"),
-                    html=j.get("htmlBody")
-                )
-        return msg
-
-class MegaAccount:
-    def __init__(self, name, password):
-        self.name = name
-        self.password = password
-
-    async def init_mail(self):
-        self.mapi = OneSecMailApi()
-        self.email = await self.mapi.get_mail()
-        return self
-
-    async def register(self):
-        if not self.email: return
-        registration = await asyncio.create_subprocess_shell(f"./megatools --register --email {self.email} --name {self.name} --password {self.password}", stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.DEVNULL)
-        stdout, _ = await registration.communicate()
-        self.verify_command = stdout.decode("utf8").strip()
-
-    async def verify(self):
-        if not self.email: return
-        content = None
-        for i in range(10):
-            if content is not None:
-                break
-            await asyncio.sleep(3)
-            for mail in await self.mapi.fetch_inbox():
-                if "MEGA" in mail.subject or "mega" in mail.text.lower() or "mega" in mail.mfrom.lower():
-                    content = mail.text
-                    break
-
-        link = _re.findall(content)
-        self.verify_command = "./"+self.verify_command.replace("@LINK@", link[0])
-
-        try:
-            verification = await asyncio.create_subprocess_shell(self.verify_command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.DEVNULL)
-            stdout, _ = await verification.communicate()
-        except Exception as e:
-            return
-
-        return (self.email, self.password)
-
-class User:
-    def __init__(self, id):
-        self.id = id
-        self.state = 0
-        self.time = time()
-
-    def setPassword(self, password):
-        self.password = password
-        self.state = 1
-
-    async def register(self, mid, cid):
-        m = False
-        while list(users.keys()).index(self.id) not in range(4):
-            await bot.edit_message_text(cid, mid, f"Your place in queue: {len(users)-2} ({round(len(users)*0.3, 1)} minutes).\n\nDetails - /help")
-            m = True
-            await asyncio.sleep(7)
-        self.state = 2
-        await bot.edit_message_text(cid, mid, "Requesting temporary email...")
-        try:
-            acc = await MegaAccount("".join(choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") for x in range(24)), self.password).init_mail()
-            await bot.edit_message_text(cid, mid, "Email received, registering...")
-            await acc.register()
-            await bot.edit_message_text(cid, mid, "Waiting for an email with an activation link...")
-            login, password = await acc.verify()
-            await bot.edit_message_text(cid, mid, "Account registered!")
-            await bot.send_message(cid, f"Login: `{login}`\nPassword: `{password}`", parse_mode=ParseMode.MARKDOWN)
-        except Exception as e:
-            print(e)
-            await bot.send_message(cid, "An unknown error occured. Please try again later.")
-        self.state = 3
-        del users[self.id]
-	
 def call_back_in_filter(data):
     return filters.create(
         lambda flt, _, query: flt.data in query.data,
@@ -257,13 +126,6 @@ def speedtest_(_,message):
     message.reply_photo(speedtest_image)
 
 
-@bot.on_message(~filters.bot & filters.text & filters.command(["account"]))
-async def command_account(_,message):
-    if message.from_user.id in users:
-        return await message.reply("You already requested an account. Wait, please.")
-    users[message.from_user.id] = User(message.from_user.id)
-    return await message.reply("Send the password you want to set for your account (min. 8 characters)")
-
 @bot.on_message(filters.regex(pattern="𝗧𝗲𝗿𝗯𝘂𝘁 𝗳𝗿𝗲𝗲 𝗰𝗼𝘂𝗿𝘀𝗲𝘀"))   
 def startprivate(_,message):
      bot.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
@@ -281,20 +143,7 @@ async def jn(_,message):
     except FloodWait as fd:
         await sleep(fd.x + 2)
     except BaseException:
-        pass
-
-@bot.on_message(~filters.bot & filters.text)
-async def message_account(_,message):
-    if message.from_user.id not in users or not message.text:
-        return
-    if users[message.from_user.id].state != 0:
-        return
-    if len(message.text.replace("\"", "").replace(" ", "")) < 8:
-        return
-    users[message.from_user.id].setPassword(message.text.replace("\\", "").replace("\"", "").replace(" ", "").replace("'", ""))
-    msg = await message.reply(f"...")
-    await users[message.from_user.id].register(msg.id, message.from_user.id)
-	
+        pass	
 @bot.on_message(filters.command('request'))
 def req(_,message):
     bot.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
