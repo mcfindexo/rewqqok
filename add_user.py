4ea9dce236a97c0b03e11ae5967f7a19545aa2c0
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-11 12:02:59 +0530

    Create add_user.py

---
+++ add_user.py
@@ -0,0 +1,8 @@
+import os
+from access_db import db
+from pyrogram import Client
+from pyrogram.types import Message
+
+async def AddUserToDatabase(bot: Client, cmd: Message):
+    if not await db.is_user_exist(cmd.from_user.id):
+        await db.add_user(cmd.from_user.id)
