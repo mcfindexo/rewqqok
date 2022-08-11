557c7bc71c72fa9ad3780ea68eb238b2bfa1e2bd
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-11 12:12:59 +0530

    Update add_user.py

--- add_user.py
+++ add_user.py
@@ -2,6 +2,7 @@ import os
 from access_db import db
 from pyrogram import Client
 from pyrogram.types import Message
+from main import bot
 
 async def AddUserToDatabase(_,message):
     if not await db.is_user_exist(cmd.from_user.id):
