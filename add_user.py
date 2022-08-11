315ae70009c409b0eef2b12ca784bbefc0b55fa1
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-11 12:18:32 +0530

    Delete add_user.py

--- add_user.py
+++
@@ -1,9 +0,0 @@
-import os
-from access_db import db
-from pyrogram import Client
-from pyrogram.types import Message
-from main import bot
-
-async def AddUserToDatabase(_,message):
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id)
