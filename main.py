0b13d212e56fcfe4fa7510e0d5498cc75e87a420
mcfindexo <110220338+mcfindexo@users.noreply.github.com>
2022-08-14 13:53:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -246,22 +246,22 @@ async def start(_,message):
         bot.send_message(-1001646296281, f"error in adding stats:\n\n{e}")
 
     if message.chat.type == "private":
-	
-    bot.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    file_id = "CAACAgIAAxkBAAEFjtZi-KftiY8llgvf-3T29MgmuMKBBQACAR4AArk8OUjrraQbd6DLgikE"
-    bot.send_sticker(message.chat.id, file_id, reply_markup=start_menu)
-    bot.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
-    text = "**🔥𝓗𝓲 𝓣𝓱𝓮𝓻𝓮 ,\n\n✅ 24 нoυr αcтιve ✓ \n⚡️ ѕυper ғαѕт reѕpoɴѕe ✓ \n\nѕerver  : нeroĸυ\nlιвrαry : pyroɢrαм\n\n/help for More Information\n\n☘️ Dᴇᴠᴇʟᴏᴘᴇʀ : @MyzoneMy\n\n🤖 вy υѕιɴɢ oυr ѕervιce yoυ мυѕт αɢree тo oυr prιvαcy polιcy 👀**"
-    reply_markup = START_BUTTON
-    message.reply_text(
-        text=text,
-        reply_markup=reply_markup,
-        disable_web_page_preview=True,
-        quote=True
-    )
+
+       bot.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+       file_id = "CAACAgIAAxkBAAEFjtZi-KftiY8llgvf-3T29MgmuMKBBQACAR4AArk8OUjrraQbd6DLgikE"
+       bot.send_sticker(message.chat.id, file_id, reply_markup=start_menu)
+       bot.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
+       text = "**🔥𝓗𝓲 𝓣𝓱𝓮𝓻𝓮 ,\n\n✅ 24 нoυr αcтιve ✓ \n⚡️ ѕυper ғαѕт reѕpoɴѕe ✓ \n\nѕerver  : нeroĸυ\nlιвrαry : pyroɢrαм\n\n/help for More Information\n\n☘️ Dᴇᴠᴇʟᴏᴘᴇʀ : @MyzoneMy\n\n🤖 вy υѕιɴɢ oυr ѕervιce yoυ мυѕт αɢree тo oυr prιvαcy polιcy 👀**"
+       reply_markup = START_BUTTON
+       message.reply_text(
+              text=text,
+              reply_markup=reply_markup,
+              disable_web_page_preview=True,
+              quote=True
+            )
     
     if not message.chat.type == "private":
-    message.reply("Hello there")
+       message.reply("Hello there")
 
 @bot.on_message(filters.command("ping"))
 async def ping(_, message):
